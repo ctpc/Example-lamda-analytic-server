403e51e1187397ec63f9417f5b2baa355884eb7c
Iman Khaghani Far <iman.khaghani@gmail.com>
2018-01-19 16:08:05 -0500

    preparing for NUCoach

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-'use strict'
-const express = require('express')
-const app = express()
+'use strict';
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`)
-})
+
+app.get('/',function(req,res){
+    res.sendFile(${__dirname}+'/index.html');
+});
+
+app.get('/misift',function(req,res){
+
+});
 
 // app.listen(3000) // <-- comment this line out from your app
 
-module.exports = app // export your app so aws-serverless-express can use it
+module.exports = app; // export your app so aws-serverless-express can use it
